630ac9c5a0b2f90e07dc7a7024c01b63171a174e
cc <votaryofturms@icloud.com>
2022-02-10 02:38:17 -0500

    validateUser

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -13,7 +13,7 @@ async function validateUserId(req, res, next) {
     const user = await User.getById(req.params.id)
     if (!user) {
       res.status(404).json({
-        message: 'not a user'
+        message: 'user not found'
       })
     }
     else {
@@ -29,7 +29,16 @@ async function validateUserId(req, res, next) {
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const { name } = req.body
+  if (!name || !name.trim()) {
+    res.status(404).json({
+      message: 'missing required name field'
+    })
+  }
+  else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
