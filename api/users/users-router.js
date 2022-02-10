e96c5b1eb77e8eb27b0f68ecc4b652d199d6c3f1
cc <votaryofturms@icloud.com>
2022-02-10 02:56:39 -0500

    post, put

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -16,21 +16,25 @@ router.get('/', (req, res) => {
 
 router.get('/:id', validateUserId, (req, res) => {
   res.json(req.user)
-  console.log(req.user)
 });
 
 router.post('/', validateUser, (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
-  console.log(req.name)
+  User.insert({ name: req.name })
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(next)
 });
 
 router.put('/:id', validateUserId, validateUser, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-  console.log(req.user)
-  console.log(req.name)
+  User.update(req.params.id, { name: req.name })
+    .then(() => {
+      return User.getById(req.params.id)
+    })
+    .then(user => {
+      res.json(user)
+    })
+    .catch(next)
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
